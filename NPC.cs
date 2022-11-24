763f184433d0f030bd1db74d82fc806eb48b5eba
Wh1zymq <118458923+Wh1zymq@users.noreply.github.com>
2022-11-24 19:59:52 +0500

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { int health = 10;
+        int newSpeed = 10;
+        health = health + newSpeed
+            print("health = health + newSpeed");
+            print(Message);
+      
+     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int speed = 10;
+     
+        
+    }
+}
